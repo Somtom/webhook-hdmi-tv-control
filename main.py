b12ebc4861033d8195df2346ecda966bd0b5750c
somtom <som_tom@web.de>
2021-05-24 15:10:11 +0200

    Fixes for shutdown mode

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def tv_on():
     app.logger.info('switching TV on')
-    os.system('sh ./tv_on.sh')
+    os.system('bash ./tv_on.sh')
     return 'TV on!'
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
--- tv_on.sh
+++ tv_on.sh
@@ -4,3 +4,4 @@ echo "on 0" | cec-client -s -d 1
 if [[ ! -z $TV_SHUTDOWN_TIMER ]]; then
     ./tv_off.sh &
 fi
+
